35fbd4e15d3b8cf639ab78279456b98f86ceb7d8
AustinHoschar <70162263+AustinHoschar@users.noreply.github.com>
2020-12-02 12:24:59 -0500

    Lab1_2

---
+++ Lab/Lab2.js
@@ -0,0 +1,24 @@
+var numberOfApples = 10;
+
+document.write("The number of apples I have is " + numberOfApples +"<br>");numberOfApples++;
+document.write("The number of apples I have is " + numberOfApples +"<br>");numberOfApples++;
+document.write("The number of apples I have is " + numberOfApples +"<br>");numberOfApples++;
+document.write("The number of apples I have is " + numberOfApples +"<br>");numberOfApples++;
+document.write("The number of apples I have is " + numberOfApples +"<br>");numberOfApples++;
+document.write("The number of apples I have is " + numberOfApples +"<br>");numberOfApples++;
+
+document.write("<br><br>")
+
+var numberOfNinja =100;
+document.write("There are "+ --numberOfNinja+ "ninjas in the room. <br>")
+
+document.write("There are "+ --numberOfNinja+ "ninjas in the room. <br>")
+
+document.write("There are "+ --numberOfNinja+ "ninjas in the room. <br>")
+
+document.write("There are "+ --numberOfNinja+ "ninjas in the room. <br>")
+
+document.write("There are "+ --numberOfNinja+ "ninjas in the room. <br>")
+
+document.write("Created by Austin Hoschar.")
+
---
+++ Lab/lab1.js
@@ -0,0 +1,19 @@
+var secondsInAMinute=60;
+var minutesInAHour=60;
+var hoursInADay=24;
+var daysInAYear =365
+
+var secondsInAHour=secondsInAMinute*minutesInAHour;
+document.write("The number of seconds in a day are "+secondsInAHour);
+document.write("<br>");
+
+var secondsInADay=secondsInAHour*hoursInADay;
+document.write("The number of seconds in a day are "+secondsInADay);
+document.write("<br>");
+
+
+var secondsInAYear= secondsInADay*daysInAYear;
+document.write("The number of seconds in a year are "+secondsInAYear);
+document.write("<br>");
+document.write("<br>");
+document.write("Created by Austin Hoschar");
